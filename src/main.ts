d3776be342976a4b20d9a26f11574547bc877a03
sangtran13 <tranngocsang147877@gmail.com>
2025-09-16 18:02:38 +0700

    feat: add pipes, DTO and validation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // root file -> entry point of ur nest js application
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // global settings
-  // env
+  // use validation pipe to validate incoming requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // to strip out any properties that are not in the dto
+      forbidNonWhitelisted: true, // to throw an error if there are extra properties
+      transform: true, // to transform the payloads to be objects of the dto classes
+      disableErrorMessages: false, // to show error messages in the response
+    }),
+  ); // to use pipes globally
 
   // start listening for requests
   await app.listen(process.env.PORT ?? 3000);
